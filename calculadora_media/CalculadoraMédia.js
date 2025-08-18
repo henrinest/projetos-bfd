38e208aa02d3447a10ce2770e24856b8ac759de3
henrinest <joaohswanderley@gmail.com>
2025-08-18 04:34:35 -0300

    Adição de sistema de validação para a Calculadora de média.

--- calculadora_media/CalculadoraMédia.js
+++ calculadora_media/CalculadoraMédia.js
@@ -3,9 +3,11 @@ const notas = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+inputNotas();
 
 //Início do input de solicitação das 4 notas dos trimestres do último ano.
-notas.question('Digite a nota do primeiro trimestre: ', (nota1) => {
+function inputNotas() {
+    notas.question('Digite a nota do primeiro trimestre: ', (nota1) => {
     notas.question('Digite a nota do segundo trimestre: ', (nota2) => {
         notas.question('Digite a nota do terceiro trimestre: ', (nota3) => {
             notas.question('Digite a nota do quarto trimestre: ', (nota4) => {
@@ -16,21 +18,38 @@ notas.question('Digite a nota do primeiro trimestre: ', (nota1) => {
                     const n3 = parseFloat(nota3);
                     const n4 = parseFloat(nota4);
 
-                //Cálculo de média da nota.
-                const media = (n1 + n2 + n3 + n4) / 4;
+                    //Validação que verifica se as notas foram escritas como strings e se elas foram digitadas em valores de 0 a 10.
+                    if (n1 > 10 || n2 > 10 || n3 > 10 || n4 > 10 || n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0 || isNaN(n1) || isNaN(n2) || isNaN(n3) || isNaN(n4)){
+                        console.log('=== Notas digitadas de maneira inválida! O sistema não aceitará textos e as notas precisam ser números entre 0 e 10! ===')
+                        console.log('=== Digite suas notas novamente: ===');
+                        console.log();
+                        inputNotas();
+                    } else {
+                        
+                        //Cálculo de média da nota.
+                        const media = (n1 + n2 + n3 + n4) / 4;
 
-                //Verificação de condicional, para saber se o aluno está aprovado ou reprovado.
-                if (media >= 7){
-                    console.log(`Sua média de nota esse ano foi de: ${media}. Você está aprovado!`);
+                        //Verificação de condicional, para saber se o aluno está aprovado ou reprovado.
+                        if (media >= 7){
+                    console.log();
+                    console.log(`=== Nota média do ano: ${media} ===`);
+                    console.log('=== Situação: Aprovado. ===');
+                    notas.close();
                 } else {
-                    console.log(`Sua média de nota esse ano foi de: ${media}. Você está reprovado!`);
+                    console.log();
+                    console.log(`=== Nota média do ano: ${media} ===`);
+                    console.log('=== Situação: Reprovado. ===');
+                    notas.close();
+                   }
                 }
-                notas.close();
             });
         });
     });
 });
+};
+
 notas.on('close', () => {
+  console.log();
   console.log('Finalizando aplicação');
   process.exit(0);
 });
