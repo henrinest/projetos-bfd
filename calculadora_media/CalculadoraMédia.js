b135f1cba25775e7614c98d4c4574cce4afd7411
henrinest <joaohswanderley@gmail.com>
2025-08-19 23:05:38 -0300

    Atualização de código da CalculadoraMédia.js

--- calculadora_media/CalculadoraMédia.js
+++ calculadora_media/CalculadoraMédia.js
@@ -1,56 +1,47 @@
-const readline = require('readline');
+const readline = require("readline");
 const notas = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-inputNotas();
 
-//Início do input de solicitação das 4 notas dos trimestres do último ano.
-function inputNotas() {
-    notas.question('Digite a nota do primeiro trimestre: ', (nota1) => {
-    notas.question('Digite a nota do segundo trimestre: ', (nota2) => {
-        notas.question('Digite a nota do terceiro trimestre: ', (nota3) => {
-            notas.question('Digite a nota do quarto trimestre: ', (nota4) => {
+ inputNotas();
 
-                    //Conversão das notas fornecidas para números.
-                    const n1 = parseFloat(nota1);
-                    const n2 = parseFloat(nota2);
-                    const n3 = parseFloat(nota3);
-                    const n4 = parseFloat(nota4);
-
-                    //Validação que verifica se as notas foram escritas como strings e se elas foram digitadas em valores de 0 a 10.
-                    if (n1 > 10 || n2 > 10 || n3 > 10 || n4 > 10 || n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0 || isNaN(n1) || isNaN(n2) || isNaN(n3) || isNaN(n4)){
-                        console.log();
-                        console.log('=== Notas digitadas de maneira inválida! O sistema não aceitará textos e as notas precisam ser números entre 0 e 10! ===')
-                        console.log('=== Digite suas notas novamente: ===');
-                        console.log();
-                        inputNotas();
-                    } else {
-                        
-                        //Cálculo de média da nota.
-                        const media = (n1 + n2 + n3 + n4) / 4;
-
-                        //Verificação de condicional, para saber se o aluno está aprovado ou reprovado.
-                        if (media >= 7){
-                    console.log();
-                    console.log(`=== Nota média do ano: ${media} ===`);
-                    console.log('=== Situação: Aprovado. ===');
-                    notas.close();
-                } else {
-                    console.log();
-                    console.log(`=== Nota média do ano: ${media} ===`);
-                    console.log('=== Situação: Reprovado. ===');
-                    notas.close();
-                   }
-                }
-            });
+//Criação da função para solicitação de múltiplas perguntas.
+function pergunta(questao) {
+    return new Promise ((resolve) => {
+        notas.question((questao), (resposta) => {
+            resolve(resposta);
         });
     });
-});
 };
 
-notas.on('close', () => {
-  console.log();
-  console.log('Finalizando aplicação, obrigado!');
-  process.exit(0);
-});
+//Solicitação de input das duas notas ao usuário.
+async function inputNotas() {
+    nota1 = await pergunta('Digite sua primeira nota: ');
+    nota2 = await pergunta('Digite sua segunda nota: ');
+
+    const n1 = parseFloat(nota1);
+    const n2 = parseFloat(nota2);
+
+    if (n1 > 10 || n2 > 10 || n1 < 0 || n2 < 0 || isNaN(n1) || isNaN(n2)) {
+        console.log();
+        console.log('=== Notas digitadas de maneira inválida! O sistema não aceitará textos e as notas precisam ser números entre 0 e 10! ===');
+        console.log('=== Digite suas notas novamente: ===');
+        console.log();
+        inputNotas();
+    } else {
+
+        //Execução do cálculo de média.
+        media = (n1 + n2) / 2;
+        console.log();
+        console.log(`== Nota média = ${media} ==`);
+
+        if (media >= 7) {
+            console.log('== Situação: Aprovado(a). ==');
+            process.exit(0);
+        } else {
+            console.log('== Situação: Reprovado(a). ==');
+            process.exit(0);
+        }
+    }
+}
